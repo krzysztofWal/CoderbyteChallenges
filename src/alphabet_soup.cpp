b4982853747098cb6778f6162f9959c2f13b22ad
Krzysztof Wal <krzysztof.j.wal@gmail.com>
2023-11-20 21:34:31 +0100

    Updated AlphabetSoup

--- src/alphabet_soup.cpp
+++ src/alphabet_soup.cpp
@@ -12,33 +12,26 @@ Optimal: o(n), achieved: o(n)
 #include <iostream>
 #include <string>
 #include <algorithm>
-#include <map>
+#include <cctype>
 
 std::string AlphabetSoup(std::string str) {
   // code goes here 
   
-  std::string tmp;
-  std::multimap<int,int> m; //char is the key, index is the value
-  bool *u_pos = new(std::nothrow) bool[str.length()]; // keep track of uppercase
-  if ( u_pos!=nullptr) {
-    for (auto i{0}; i<str.length(); i++) {
-      if (static_cast<int>(str.at(i)) < 97) {u_pos[i] = 1; m.insert(std::pair<int,int>(str.at(i)+32,i));}
-        else {u_pos[i] = 0;  m.insert(std::pair<int,int>(str.at(i),i));}
-    }
-    for (auto it{m.begin()};it!=m.end();it++){
-      tmp += static_cast<char>((u_pos[it->second]) ? (it->first)-32 : it->first);
-    }
-    
-    delete[] u_pos;
-    u_pos = nullptr;
+  std::sort(str.begin(), str.end(), [](char a, char b){
 
-    return tmp;
-  } else {
-    return "Error allocating";
-  }
+    // HeLloHellOhello - >OooLlllllHHheee
+    char lower_a = std::tolower(a);
+    char lower_b = std::tolower(b);
+
+    if (lower_a == lower_b) {
+      // if lower are the same return the smaller number (capital letter that is)
+      return a < b;
+    } else {
+      // if the letters are different
+      return lower_a > lower_b;
+    }
+  });
   
-  // well if there are no upper cases
-  // std::sort(str.begin(),str.end());
   return str;
 }
 
