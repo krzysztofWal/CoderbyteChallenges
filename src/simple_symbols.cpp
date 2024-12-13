525ea4540a3ed300ac038197b4221c4c5da1d5b4
Krzysztof Wal <krzysztof.j.wal@gmail.com>
2024-12-13 23:05:33 +0100

    nasty little regexeses my precious

--- src/simple_symbols.cpp
+++ src/simple_symbols.cpp
@@ -14,7 +14,10 @@ Optimal: o(n), achieved: o(n)
 
 #include <iostream>
 #include <string>
+#define SAY_YES_TO_REGEX 
+// #define PRINT_SIMPLE_SYMBOLS
 
+#ifndef SAY_YES_TO_REGEX
 std::string SimpleSymbols(std::string str) {
   
   // code goes here
@@ -27,6 +30,41 @@ std::string SimpleSymbols(std::string str) {
   }
   return "true";
 }
+#else
+#include <regex>
+
+/* This one not tested very thoughrougly, and given what I've read about implementation of regexes in standard libray
+it is probably slower than the solution above xD */
+std::string SimpleSymbols(std::string str) {
+
+  std::regex reg, reg2;
+  std::string tmp;
+  const char toReplace[] = "*";
+
+  try {
+    /* because standard library regex are a funny thing you cannot use lookaheads and lookbacks */
+    reg = std::regex(R"((?:(\+[a-z]){1,}\+))", std::regex_constants::icase);
+    reg2 = std::regex("[a-z]", std::regex_constants::icase);
+  } catch (std::regex_error &e) {
+    std::cerr << "\tError: " << e.what() << ", code: " << e.code() << std::endl; 
+    throw std::runtime_error("Oopsie daisy!");
+  }
+  /* remove single letters surrounded by '+' */
+  std::regex_replace(std::back_inserter(tmp), str.cbegin(), str.cend(),reg , toReplace);
+
+  #ifdef PRINT_SIMPLE_SYMBOLS
+    std::cout << "SimpleSymbols(): " << tmp << std::endl;
+  #endif
+
+  /* if there are any left now, return false */
+  if (std::regex_search(tmp, reg2)) {
+    return "false";
+  } else {
+    return "true";
+  }
+  
+}
+#endif
 
 int main(void) { 
   // keep this function call here
